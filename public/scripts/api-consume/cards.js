238dabe6f9fb848f0b24287f881b9f27e1ebbef0
Josélio Samy <joseliobackup@gmail.com>
2021-03-25 02:02:55 -0300

    calculo api

--- public/scripts/api-consume/cards.js
+++ public/scripts/api-consume/cards.js
@@ -1,51 +1,9 @@
 async function consumeCards(){
     try{
-        // FÍSICA -------------------------------
-
-        if(window.location.pathname == "/fisica/"){
-            var response = await fetch('https://api-seeclasses.vercel.app/fisica')
-            
-            const data = await response.json();
-            show(data)
-        }
-
-        // END FÍSICA -------------------------------
-        // CÁLCULO -------------------------------
+        var response = await fetch('https://api-seeclasses.vercel.app/')
         
-        if(window.location.pathname == "/calculo/"){
-            response = await fetch('https://api-seeclasses.vercel.app/calculo')
-            
-            const data = await response.json();
-            show(data)
-        }
-        // END CÁLCULO -------------------------------
-        // CÁLCULO VETORIAL -------------------------------
-        
-        if(window.location.pathname == "/calculo-vetorial/"){
-            response = await fetch('https://api-seeclasses.vercel.app/calculo-vetorial')
-            
-            const data = await response.json();
-            show(data)
-        }
-        // END CÁLCULO VETORIAL -------------------------------
-        // INTRO COMPUTADOR -------------------------------
-        
-        if(window.location.pathname == "/intro-computador/"){
-            response = await fetch('https://api-seeclasses.vercel.app/intro-computador')
-            
-            const data = await response.json();
-            show(data)
-        }
-        // END INTRO COMPUTADOR -------------------------------
-        // INTRO PROGRAMAÇÃO -------------------------------
-        
-        if(window.location.pathname == "/intro-programacao/"){
-            response = await fetch('https://api-seeclasses.vercel.app/intro-programacao')
-            
-            const data = await response.json();
-            show(data)
-        }
-        // END INTRO PROGRAMAÇÃO -------------------------------
+        const data = await response.json();
+        show(data)
     }
 
     catch(error){
@@ -54,28 +12,30 @@ async function consumeCards(){
 }
 consumeCards();
 
-function show(cards){
+if(window.location.pathname == "/calculo/"){
+function show(items){
     let output = "";
 
-    for(let card of cards){
+    for(let item of items){
         output += `
         <div class="card">
         <div class="card_name">
-            <h2>${card.cardHeader.title}</h2>
+            <h2>${card.calculo.card.cardHeader.title}</h2>
         </div>
         <div class="card_body">
             <div class="thumbl">
-                <img src="${card.cardBody.cardImage}" alt="${card.cardBody.imageAlt}">
+                <img src="${card.calculo.card.cardBody.cardImage}" alt="${card.calculo.card.cardBody.imageAlt}">
             </div>
             <div class="date">
-                <p>${card.cardBody.date}</p>
+                <p>${card.calculo.card.cardBody.date}</p>
             </div>
             <div class="redirect">
-                <a href="${card.cardBody.videoHref}" target="_blank"><i class='bx bx-play'></i>Assistir</a>
+                <a href="${card.calculo.card.cardBody.videoHref}" target="_blank"><i class='bx bx-play'></i>Assistir</a>
             </div>
         </div>
     </div><!-- CARD -->
         `
     }
     document.querySelector('.cards_wrapper').insertAdjacentHTML("afterbegin", output)
+}
 }
